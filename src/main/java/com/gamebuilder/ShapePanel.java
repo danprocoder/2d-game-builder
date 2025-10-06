3671b6b22e0c0c04ec6dcf1651080c3aa2f5b671
Daniel Austin <danielaustin718@gmail.com>
2025-10-06 20:33:41 +0100

    Create color picker dialog

--- src/main/java/com/gamebuilder/ShapePanel.java
+++ src/main/java/com/gamebuilder/ShapePanel.java
@@ -1,13 +1,145 @@
 package com.gamebuilder;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
-import com.gamebuilder.GameBuilder;
+import com.gamebuilder.model.CanvasModel;
+import com.gamebuilder.model.CanvasModelUpdateListener;
+
+class ColorPickerDialog extends JDialog {
+    private CanvasModel model;
+    private Color color;
+    
+    ColorPickerDialog(CanvasModel model) {
+        this.model = model;
+        this.color = new Color(
+            model.getColor().getRed(),
+            model.getColor().getGreen(),
+            model.getColor().getBlue(),
+            model.getColor().getAlpha()
+        );
+
+        showForm();
+
+        setTitle("Pick a color");
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private void showForm() {
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+
+        // The big panel showing the chosen color
+        JPanel panel = new JPanel();
+        panel.setBackground(this.color);
+        panel.setPreferredSize(new Dimension(300, 300));
+
+        add(panel);
+
+        // Input fields
+        JPanel rgba = new JPanel();
+        rgba.setLayout(new BoxLayout(rgba, BoxLayout.X_AXIS));
+        
+        rgba.add(new JLabel("R"));
+        JSpinner rField = new JSpinner(new SpinnerNumberModel(this.color.getRed(), 0, 255, 1));
+        rField.addChangeListener(e -> {
+            int r = (int) rField.getValue();
+            color = new Color(r, color.getGreen(), color.getBlue(), color.getAlpha());
+            panel.setBackground(color);
+        });
+        rgba.add(rField);
+
+        rgba.add(new JLabel("G"));
+        JSpinner gField = new JSpinner(new SpinnerNumberModel(this.color.getGreen(), 0, 255, 1));
+        gField.addChangeListener(e -> {
+            int g = (int) gField.getValue();
+            color = new Color(color.getRed(), g, color.getBlue(), color.getAlpha());
+            panel.setBackground(color);
+        });
+        rgba.add(gField);
+
+        rgba.add(new JLabel("B"));
+        JSpinner bField = new JSpinner(new SpinnerNumberModel(this.color.getBlue(), 0, 255, 1));
+        bField.addChangeListener(e -> {
+            int b = (int) bField.getValue();
+            color = new Color(color.getRed(), color.getGreen(), b, color.getAlpha());
+            panel.setBackground(color);
+        });
+        rgba.add(bField);
+
+        rgba.add(new JLabel("A"));
+        JSpinner aField = new JSpinner(new SpinnerNumberModel(this.color.getAlpha(), 0, 255, 1));
+        aField.addChangeListener(e -> {
+            int a = (int) aField.getValue();
+            color = new Color(color.getRed(), color.getGreen(), color.getBlue(), a);
+            panel.setBackground(color);
+        });
+        rgba.add(aField);
+
+        add(rgba);
+
+        // Bottom buttons
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
+        JButton cancelButton = new JButton("Cancel");
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(e -> {
+            model.setColor(this.color);
+
+            setVisible(false);
+        });
+
+        buttons.add(cancelButton);
+        buttons.add(okButton);
+
+        add(buttons);
+    }
+
+    
+}
+
+class ColorPicker extends JPanel implements ActionListener, CanvasModelUpdateListener {
+    private CanvasModel model;
+
+    private JPanel selectedColor;
+
+    ColorPicker(CanvasModel model) {
+        this.model = model;
+        this.model.addUpdateListener(this);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        selectedColor = new JPanel();
+        selectedColor.setPreferredSize(new Dimension(50, 50));
+        selectedColor.setBackground(model.getColor());
+        add(selectedColor);
+
+        JButton button = new JButton("Change Color");
+        button.addActionListener(this);
+        add(button);
+    }
+
+    @Override
+    public void onCanvasModelUpdated(CanvasModel model) {
+        selectedColor.setBackground(model.getColor());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ColorPickerDialog dialog = new ColorPickerDialog(model);
+        dialog.setVisible(true);
+    }
+}
 
 public class ShapePanel extends JPanel {
     final static String MOVE_TOOL = "move";
@@ -22,7 +154,7 @@ public class ShapePanel extends JPanel {
     public ShapePanel(GameBuilder gameBuilder) {
         this.gameBuilder = gameBuilder;
 
-        setPreferredSize(new Dimension(100, 400));
+        setPreferredSize(new Dimension(150, 400));
         addButtons();
     }
 
@@ -78,5 +210,6 @@ public class ShapePanel extends JPanel {
         add(polygonTool);
         add(circleTool);
         add(rectTool);
+        add(new ColorPicker(gameBuilder.getModel()));
     }
 }
