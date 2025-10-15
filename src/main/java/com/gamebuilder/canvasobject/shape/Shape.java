4111a0d66beed66d8c3c4dfadf8c170958a2f2ce
Daniel Austin <danielaustin718@gmail.com>
2025-10-15 08:09:15 +0100

    Shape class implements base selectable interface

--- src/main/java/com/gamebuilder/canvasobject/shape/Shape.java
+++ src/main/java/com/gamebuilder/canvasobject/shape/Shape.java
@@ -1,7 +1,16 @@
 package com.gamebuilder.canvasobject.shape;
 
 import com.gamebuilder.canvasobject.CanvasObject;
+import com.gamebuilder.model.Selectable;
 
-public abstract class Shape extends CanvasObject {
-    
+public abstract class Shape extends CanvasObject implements Selectable {
+    private boolean selected = false;
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public boolean getSelected() {
+        return this.selected;
+    }
 }
