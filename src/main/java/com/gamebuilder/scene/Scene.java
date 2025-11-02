b4f31ac1e8944ea3e8c644978db22bff57b21c54
Daniel Austin <danielaustin718@gmail.com>
2025-11-02 13:07:54 +0000

    Add objects and events for scenes to xml

--- src/main/java/com/gamebuilder/scene/Scene.java
+++ src/main/java/com/gamebuilder/scene/Scene.java
@@ -1,7 +1,12 @@
 package com.gamebuilder.scene;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.gamebuilder.canvasobject.CanvasObject;
 import com.gamebuilder.model.Asset;
 import com.gamebuilder.model.CanvasModel;
+import com.gamebuilder.model.Event;
 import com.gamebuilder.service.CanvasService;
 
 public class Scene {
@@ -9,10 +14,11 @@ public class Scene {
     private String name;
     private CanvasService canvasService;
     private Asset backgroundMusic;
+    private ArrayList<Event> events = new ArrayList<Event>();
     private boolean isActive;
 
     public Scene(String name) {
-        this(java.util.UUID.randomUUID().toString(), name);
+        this(UUID.randomUUID().toString(), name);
     }
 
     public Scene(String id, String name) {
@@ -42,18 +48,48 @@ public class Scene {
         this.isActive = isActive;
     }
 
+    public void addEvent(Event event) {
+        this.events.add(event);
+    }
+
+    public ArrayList<Event> getEvents() {
+        return this.events;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
+    }
+
+    public CanvasService getCanvasService() {
+        return this.canvasService;
+    }
+
     public String toXml() {
         StringBuilder sb = new StringBuilder();
         sb.append(
             String.format(
-                "\n<Scene id=\"%s\" name=\"%s\" backgroundMusicId=\"%s\">",
+                "\n  <Scene id=\"%s\" name=\"%s\" backgroundMusicId=\"%s\">",
                 this.id,
                 this.name,
                 this.backgroundMusic != null ? this.backgroundMusic.getId() : ""
             )
         );
-        // TODO: serialize scene objects
-        sb.append("\n</Scene>");
+
+        sb.append("\n    <Objects>");
+        for (CanvasObject object: this.canvasService.getObjects()) {
+            sb.append(object.toXml());
+        }
+        sb.append("\n    </Objects>");
+
+        if (this.events.size() > 0) {
+            sb.append("\n    <Events>");
+            for (var event: this.events) {
+                sb.append(event.toXml());
+            }
+            sb.append("\n    </Events>");
+        }
+
+        sb.append("\n  </Scene>");
         return sb.toString();
     }
 }
