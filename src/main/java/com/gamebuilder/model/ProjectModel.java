1e5c1aec6b901f9f2be4f6361dcee5a7a40b7a0e
Daniel Austin <danielaustin718@gmail.com>
2025-11-02 09:14:32 +0000

    Remove scene code from ProjectModel

--- src/main/java/com/gamebuilder/model/ProjectModel.java
+++ src/main/java/com/gamebuilder/model/ProjectModel.java
@@ -13,40 +13,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import com.gamebuilder.GameBuilder;
-import com.gamebuilder.scene.Scene;
-
 public class ProjectModel {
-  GameBuilder gameBuilder;
   Project project = null;
   ArrayList<ProjectModelUpdateListener> updateListeners = new ArrayList<ProjectModelUpdateListener>();
-
-  ArrayList<Scene> scenes = new ArrayList<>();
-  int currentSceneIndex = -1;
-
-  public ProjectModel(GameBuilder gameBuilder) {
-    this.gameBuilder = gameBuilder;
-  }
-
-  public ArrayList<Scene> getScenes() {
-    return this.scenes;
-  }
-
-  public int addScene(Scene scene) {
-    this.scenes.add(scene);
-    return this.scenes.size() - 1;
-  }
-
-  public void setCurrentSceneIndex(int index) {
-    this.currentSceneIndex = index;
-  }
-
-  public Scene getCurrentScene() {
-    if (this.currentSceneIndex < 0 || this.currentSceneIndex >= this.scenes.size()) {
-      return null;
-    }
-    return this.scenes.get(this.currentSceneIndex);
-  }
 
   /**
    * Load a project from file
@@ -56,8 +25,7 @@ public class ProjectModel {
   public Project loadProject(String path)
       throws IOException, ParserConfigurationException, SAXException {
     FileInputStream fis = new FileInputStream(
-      Paths.get(path, ProjectTemplate.getProjectFile()).toFile()
-    );
+        Paths.get(path, ProjectTemplate.getProjectFile()).toFile());
     ByteArrayInputStream in = new ByteArrayInputStream(fis.readAllBytes());
 
     DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
