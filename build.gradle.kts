cde86bcc15318ad75a48f36842c4e19800277fa9
Daniel Austin <danielaustin718@gmail.com>
2025-11-08 16:22:52 +0000

    Add junit to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
     // implementation("com.googlecode.soundlibs:mp3spi:1.9.5.4")
     // implementation("com.googlecode.soundlibs:tritonus-share:0.3.7.4")
     // implementation("com.googlecode.soundlibs:jlayer:1.0.1.4")
+    
+    // Test dependencies
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 application {
@@ -25,4 +29,8 @@ application {
         "--add-exports=java.desktop/sun.awt=ALL-UNNAMED",
         "--add-opens=java.desktop/sun.awt=ALL-UNNAMED"
     )
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
