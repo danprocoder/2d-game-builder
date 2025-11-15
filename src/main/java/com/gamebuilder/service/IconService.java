af61c510ab498127945b5de7ed7783a79223300c
Daniel Austin <danielaustin718@gmail.com>
2025-11-15 19:48:59 +0000

    Fetch icons at specific width and height

--- src/main/java/com/gamebuilder/service/IconService.java
+++ src/main/java/com/gamebuilder/service/IconService.java
@@ -11,14 +11,14 @@ import javax.swing.ImageIcon;
 public class IconService {
     static HashMap<String, ImageIcon> iconCache = new HashMap<String, ImageIcon>();
 
-    public static ImageIcon getIcon(String path) throws IOException {
+    public static ImageIcon getIcon(String path, int w, int h) throws IOException {
         path = "/icons" + path;
         if (iconCache.containsKey(path)) {
             return iconCache.get(path);
         }
 
         BufferedImage img = ImageIO.read(IconService.class.getResourceAsStream(path));
-        Image scaledImg = img.getScaledInstance(24, 24, Image.SCALE_SMOOTH);
+        Image scaledImg = img.getScaledInstance(w, h, Image.SCALE_SMOOTH);
         ImageIcon icon = new ImageIcon(scaledImg);
         iconCache.put(path, icon);
         return icon;
